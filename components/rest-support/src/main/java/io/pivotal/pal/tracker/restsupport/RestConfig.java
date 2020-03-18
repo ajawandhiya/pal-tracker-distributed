479dc2ce8a58dc8ede57835302ce71a4439658e8
ajawandhiya <abhishek.jawandhiya@cognizan.com>
2020-03-18 17:03:46 -0500

    Circuit Breaker

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -3,6 +3,7 @@ package io.pivotal.pal.tracker.restsupport;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 //  import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestOperations;
@@ -13,11 +14,11 @@ import org.springframework.web.client.RestTemplate;
 
 public class RestConfig {
 
-
-    @Bean
-    public RestOperations restOperations() {
-    return new RestTemplate();
-    }
+  //  @LoadBalanced
+    //@Bean
+    //public RestOperations restOperations() {
+    //return new RestTemplate();
+    //}
 
     @Bean
     public ObjectMapper objectMapper() {

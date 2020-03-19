3fd1a898278d13fbebc254137214ee6af8e691fd
ajawandhiya <abhishek.jawandhiya@cognizan.com>
2020-03-19 18:12:49 -0500

    Circuit Breaker

--- components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
+++ components/backlog/src/main/java/io/pivotal/pal/tracker/backlog/ProjectClient.java
@@ -1,36 +1,65 @@
 package io.pivotal.pal.tracker.backlog;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
+
+
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
+
 import org.springframework.web.client.RestOperations;
 
+
+
 import java.util.Map;
+
 import java.util.concurrent.ConcurrentHashMap;
+
+
 
 public class ProjectClient {
 
+
+
     private final Logger logger = LoggerFactory.getLogger(getClass());
+
     private final Map<Long, ProjectInfo> projectsCache = new ConcurrentHashMap<>();
+
     private final RestOperations restOperations;
+
     private final String endpoint;
 
+
+
     public ProjectClient(RestOperations restOperations, String registrationServerEndpoint) {
+
         this.restOperations = restOperations;
+
         this.endpoint = registrationServerEndpoint;
+
     }
 
-    @CircuitBreaker(name = "project", fallbackMethod = "getProjectFromCache")
+
+
+    @HystrixCommand(fallbackMethod = "getProjectFromCache")
+
     public ProjectInfo getProject(long projectId) {
-        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
 
-        projectsCache.put(projectId, project);
+        return restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
 
-        return project;
     }
 
-    public ProjectInfo getProjectFromCache(long projectId, Throwable cause) {
+
+
+    public ProjectInfo getProjectFromCache(long projectId) {
+
         logger.info("Getting project with id {} from cache", projectId);
+
         return projectsCache.get(projectId);
+
     }
+
 }
